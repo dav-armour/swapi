41b62173fd28a627e45db6b11492916e0c78a497
David Armour <15181647+dav-armour@users.noreply.github.com>
2019-06-01 15:41:11 +1000

    add ability to favourite films on films table

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
-# README
+# Description
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Produce a UI for the Star Wars API built using Ruby on Rails
 
-Things you may want to cover:
+# Goal
 
-* Ruby version
+Consume film and character information from SWAPI and display it in a clean, performant user interface.
 
-* System dependencies
+# Acceptance criteria
 
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+- Home screen contains a list of films pulled from the SWAPI
+- List should be searchable
+- Films can be favourited, this state should be persisted using local storage
+- Favourited films should appear at the top of the list
+- A stylised user prompt/alert should show when a film is favourited
+- Favourited films can be unfavourited
+- Film list items can be clicked to show individual film page
+- Film page should show all the information pulled from the SWAPI endpoint for an individual film
+- Film page should contain a back to home link
+- The list of characters should display a tooltip when a list item is hovered
+- The tooltip should show the character's bio pulled from the SWAPI endpoint for people
+- Provide public GitHub repo to source code
+- Host the finished project on Heroku, Surge or similar
+- This test is focused on demonstrating Ruby on Rails, do not sweat the styling too much and feel free to use a framework like Bootstrap or similar
+- Bonus points (not required)
+- Use the tooltip UI to show additional information pertaining to the film like vehicles, spaceships etc
+- Demonstrate creative and performant ways of fetching information from SWAPI
+- Provide a documented readme inside the GitHub repo explaining the technology used, the setup process and other relevant information
--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -1,18 +1,79 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-document.addEventListener("turbolinks:load", function() {
+document.addEventListener("turbolinks:load", () => {
+  enableFavourites();
+  enableSearch();
+});
+
+function enableSearch() {
   const searchInput = document.getElementById("searchInput");
 
-  searchInput.addEventListener("keyup", () => {
+  searchInput.addEventListener("keyup", e => {
     const searchValue = searchInput.value.toUpperCase();
-    const titleNodes = document.querySelectorAll("#filmTable td:first-of-type");
-    titleNodes.forEach(node => {
-      if (node.innerText.toUpperCase().indexOf(searchValue) > -1) {
-        node.parentNode.style.display = "";
-      } else {
-        node.parentNode.style.display = "none";
+
+    const filmTable = document.getElementById("filmTable");
+    const rows = filmTable.getElementsByTagName("tr");
+    for (let row of rows) {
+      const cell = row.getElementsByTagName("td")[0];
+      if (cell) {
+        const title = cell.textContent;
+        if (title.toUpperCase().indexOf(searchValue) > -1) {
+          row.style.display = "";
+        } else {
+          row.style.display = "none";
+        }
       }
-    });
+    }
   });
-});
+}
+
+function enableFavourites() {
+  const filmTable = document.getElementById("filmTable");
+  const tbody = filmTable.lastElementChild;
+
+  // Check local storage for favourite films and update DOM
+  const favFilms = JSON.parse(localStorage.getItem("favFilms")) || [];
+  const rows = tbody.getElementsByTagName("tr");
+  for (let row of rows) {
+    const episodeId = row.dataset["episode_id"];
+    if (favFilms.indexOf(episodeId) > -1) {
+      row.lastElementChild.textContent = "Yes";
+      tbody.prepend(tbody.removeChild(row));
+    }
+  }
+
+  filmTable.addEventListener("click", e => {
+    if (e.target.className.indexOf("fav-cell") > -1) {
+      handleFavClick(e);
+    } else {
+      handleRowClick(e);
+    }
+  });
+}
+
+function handleFavClick(e) {
+  const tr = e.target.parentNode;
+  const tbody = tr.parentNode;
+  const episodeId = tr.dataset["episode_id"];
+  const favFilms = JSON.parse(localStorage.getItem("favFilms")) || [];
+  const index = favFilms.indexOf(episodeId);
+  if (index > -1) {
+    favFilms.splice(index, 1);
+    e.target.textContent = "No";
+    tbody.insertBefore(
+      tbody.removeChild(tr),
+      tbody.childNodes[favFilms.length]
+    );
+  } else {
+    favFilms.push(episodeId);
+    e.target.textContent = "Yes";
+    tbody.prepend(tbody.removeChild(tr));
+  }
+  localStorage.setItem("favFilms", JSON.stringify(favFilms));
+}
+
+function handleRowClick(e) {
+  const episodeId = e.target.parentNode.dataset["episode_id"];
+  // window.location = `/films/${episodeId}`;
+}
