d3d0d05fbe4bcd26f3cb88747f8b547a6e19c563
David Armour <15181647+dav-armour@users.noreply.github.com>
2019-06-02 16:57:15 +1000

    change parallel to processes instead of threads

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -6,7 +6,7 @@ class FilmsController < ApplicationController
   def show
     @film = SwapiService.films(params[:id])
     types = { characters: 9, planets: 7, starships: 4, vehicles: 4, species: 4  }
-    Parallel.each(types, in_threads: 5) do |type, limit|
+    Parallel.each(types, in_processes: 5) do |type, limit|
       if @film[type.to_s].count > limit
         all = SwapiService.send(type.to_s)
         results = all.select do |char|
