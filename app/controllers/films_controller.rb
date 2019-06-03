7b8eccd996cac4044fb68e137e1189f8e949a362
David Armour <15181647+dav-armour@users.noreply.github.com>
2019-06-03 13:21:45 +1000

    fix variable names and add comments to film controller

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -5,17 +5,19 @@ class FilmsController < ApplicationController
 
   def show
     @film = SwapiService.films(params[:id])
+    # Hash of resource types and total pages of data
     types = { characters: 9, planets: 7, starships: 4, vehicles: 4, species: 4  }
     Parallel.each(types, in_threads: 5) do |type, limit|
+      # Get all records and filter if it will be less api requests
       if @film[type.to_s].count > limit
         all = SwapiService.send(type.to_s)
-        results = all.select do |char|
-            @film[type.to_s].include?(char['url'])
+        results = all.select do |record|
+            @film[type.to_s].include?(record['url'])
         end
-      else
+      else # Otherwise get individual records
         results = []
-        @film[type.to_s].each do |ship|
-          match = ship.match /\/(\w+)\/$/
+        @film[type.to_s].each do |url|
+          match = url.match /\/(\w+)\/$/
           results << SwapiService.send(type.to_s, match[1])
         end
       end
